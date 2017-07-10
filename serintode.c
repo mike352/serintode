2beff9d0fd909a19f2b00a6d8975b448e302e32b
Mike <assis.michael@gmail.com>
2017-07-10 18:35:14 +1000

    Changed variable names, added a gitignore file

--- serintode.c
+++ serintode.c
@@ -7,23 +7,25 @@
 #include "iml.h"
 
 //Check things with Valgrind
-//Compile: gcc -Wall imltest.c -o imltest -liml -lcblas -lgmp -lm
+//Compile: gcc -Wall serintode.c -o serintode -liml -lcblas -lgmp -lm
 
 int main()
 {
     long const MAX_DIGITS=300L; /*This needs to be input*/
     long const NUM_INPUT=100L; /*This needs to be input. If changing type, change n declaration and n equating twice below*/ 
+    long const MAX_ODE_ORDER=2L; /*This needs to be input or automated*/
+    long const MAX_POLY_ORDER=4L; /*This needs to be input or automated*/
     long const ROWS=2L;
     long const COLUMNS=2L;
-    long n,m,i,j;
-    long s;
+    long i,j,r=ROWS,c=COLUMNS;
+    long nulldim;
     char input_string[MAX_DIGITS+1L];
-    mpz_t *v, *r;
+    mpz_t *M, *N;
     FILE* fid=NULL;
     char *fgcheck;
     
 
-    v = (mpz_t*) malloc(NUM_INPUT*sizeof(mpz_t));
+    M = (mpz_t*) malloc(NUM_INPUT*sizeof(mpz_t));
     
     fid = fopen("kolakoski.txt","r");
     if (fid==NULL)
@@ -32,7 +34,7 @@ int main()
         return 1;
     }
     
-    for (n=0L;n<NUM_INPUT;n++)
+    for (i=0L;i<NUM_INPUT;i++)
     {
         fgcheck = fgets(input_string,MAX_DIGITS,fid);
         if (fgcheck==NULL)
@@ -45,50 +47,49 @@ int main()
             {
                 printf("Error while reading the file. %s\n",strerror(errno));
             }
-            for (m=0L;m<n+1L;m++)
+            for (j=0L;j<i+1L;j++)
             {
-                mpz_clear(v[m]);
+                mpz_clear(M[j]);
             }
             fclose(fid);
             return 1;
         }    
         input_string[strcspn(input_string,"\r\n")]='\0'; /*Remove trailing newline characters, May not be necessary*/
-        mpz_init_set_str(v[n],input_string,10);
-        //mpz_out_str(NULL,10,v[n]);
+        mpz_init_set_str(M[i],input_string,10);
+        //mpz_out_str(NULL,10,M[i]);
         //printf("\n");
     }
     
-    n=ROWS; m=COLUMNS;
-    mpz_set_si(v[0],2); //1
-    mpz_set_si(v[1],1); //2
-    mpz_set_si(v[2],-4); //3
-    mpz_set_si(v[3],-2); //6
-    s = nullspaceMP (n, m, v, &r);
+    mpz_set_si(M[0],2); //1
+    mpz_set_si(M[1],1); //2
+    mpz_set_si(M[2],-4); //3
+    mpz_set_si(M[3],-2); //6
+    nulldim = nullspaceMP (r, c, M, &N);
     fprintf (stdout, "Dimension of nullspace: ");
-    fprintf (stdout, " %ld\n", s);
-    for (i = 0; i < m; i++)
+    fprintf (stdout, " %ld\n", nulldim);
+    for (i = 0; i < c; i++)
         {
-        for (j = 0; j < s; j++)
-            gmp_fprintf (stdout, "  %Zd", r[i * s + j]);
+        for (j = 0; j < nulldim; j++)
+            gmp_fprintf (stdout, "  %Zd", N[i * nulldim + j]);
         fprintf (stdout, "\n");
         }
     
     //Factorial test
-    mpz_fac_ui(v[2],100);
-    //mpz_out_str(NULL,10,v[2]);
+    mpz_fac_ui(M[2],100);
+    //mpz_out_str(NULL,10,M[2]);
     //printf("\n");
     
     /*Clearing mpz variables and closing file*/
-    for (n=0L;n<NUM_INPUT;n++)
+    for (i=0L;i<NUM_INPUT;i++)
     {
-        mpz_clear(v[n]);
+        mpz_clear(M[i]);
     }
-    for (n=0L;n<ROWS*s;n++)
+    for (i=0L;i<r*nulldim;i++)
     {
-        mpz_clear(r[n]);
+        mpz_clear(N[i]);
     }
-    free(v);
-    free(r);
+    free(M);
+    free(N);
     fclose(fid);
     return 0;
 }
